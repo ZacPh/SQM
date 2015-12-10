39024fdd96d3c3877adf870c2969a3ee61cd50b9
ZacPh <zacphillipshemming@googlemail.com>
2015-12-10 12:30:43 +0000

    Testing Ant - added suite and changed classpath

---
+++ src/g54ubi/chat/server/AllTests.java
@@ -0,0 +1,12 @@
+package g54ubi.chat.server;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MyTestTest.class })
+public class AllTests
+{
+
+}
