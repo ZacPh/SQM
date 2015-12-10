476863b30b2bfefe6200a932fd51c9ff6452b0b8
ZacPh <zacphillipshemming@googlemail.com>
2015-12-10 12:23:50 +0000

    added build.xml and junit eclipse

---
+++ src/g54ubi/chat/server/MyTestTest.java
@@ -0,0 +1,17 @@
+package g54ubi.chat.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTestTest
+{
+
+	@Test
+	public void testFailure() throws Exception
+	{
+		
+	}
+	
+
+}
